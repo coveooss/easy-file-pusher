f139113cc37a09305edf0fad40b47dffeb42982f
Ronald Quirion <rquirion@coveo.com>
2020-08-17 16:01:18 -0400

    Easy File Pusher. [CT-6736]

--- src/Coveo.Connectors.EasyFilePusher/Program.cs
+++ src/Coveo.Connectors.EasyFilePusher/Program.cs
@@ -1,15 +1,160 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using CommandLine;
+using Coveo.Connectors.Utilities.PlatformSdk;
+using Coveo.Connectors.Utilities.PlatformSdk.Config;
+using Coveo.Connectors.Utilities.PlatformSdk.Helpers;
+using Coveo.Connectors.Utilities.PlatformSdk.Model.Document;
 
 namespace Coveo.Connectors.EasyFilePusher
 {
     /// <summary>
-    /// Entry point.
+    /// Main class of the program.
     /// </summary>
     public class Program
     {
-        private static void Main(string[] args)
+        private const string INVALID_CLOUD_REGION = "Invalid cloud region.";
+        private const string INVALID_CLOUD_ENVIRONMENT = "Invalid cloud environment.";
+        private const string HIPAA_INVALID_FOR_EUWEST1 = "The " + nameof(CloudEnvironment.Hipaa) + " environment is not available for the region " + nameof(CloudRegion.EuWest1) + ".";
+
+        /// <summary>
+        /// Entry point of the program.
+        /// </summary>
+        /// <param name="p_Args"></param>
+        private static void Main(string[] p_Args)
         {
-            Console.WriteLine("Hello World!");
+            new Parser(settings => {
+                settings.CaseInsensitiveEnumValues = true;
+                settings.HelpWriter = Console.Out;
+            }).ParseArguments<ProgramArguments>(p_Args).WithParsed(parsedArgs => {
+                IndexFiles(parsedArgs);
+            });
+        }
+
+        /// <summary>
+        /// Indexes the files located in the specified folder.
+        /// </summary>
+        /// <param name="p_Args">Parsed command-line arguments.</param>
+        private static void IndexFiles(ProgramArguments p_Args)
+        {
+            ICoveoPlatformConfig platformConfig = new CoveoPlatformConfig(GetPushApiUrl(p_Args),  GetPlatformApiUrl(p_Args), p_Args.ApiKey, p_Args.Organization);
+            using (ICoveoPlatformClient platformClient = new CoveoPlatformClient(platformConfig)) {
+                IList<PushDocument> documentBatch = new List<PushDocument>();
+                foreach (FileInfo fileInfo in new DirectoryInfo(p_Args.Folder).EnumerateFiles(p_Args.Include, p_Args.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)) {
+                    PushDocument document = new PushDocument(new Uri(fileInfo.FullName).AbsoluteUri) {
+                        ModifiedDate = fileInfo.LastWriteTimeUtc
+                    };
+                    PushDocumentHelper.SetContentFromFile(document, fileInfo.FullName);
+                    documentBatch.Add(document);
+
+                    if (documentBatch.Count >= p_Args.BatchSize) {
+                        // Push this batch of documents.
+                        SendBatch(platformClient, documentBatch, p_Args.SourceId);
+                    }
+                }
+
+                // Send the (partial) final batch of documents.
+                SendBatch(platformClient, documentBatch, p_Args.SourceId);
+            }
+        }
+
+        /// <summary>
+        /// Sends a batch of documents to the platform.
+        /// </summary>
+        /// <param name="p_PlatformClient">The instance of <see cref="ICoveoPlatformClient"/> to use.</param>
+        /// <param name="p_DocumentBatch">The batch of documents to send.</param>
+        /// <param name="p_SourceId">ID of the source in which to push documents.</param>
+        private static void SendBatch(ICoveoPlatformClient p_PlatformClient,
+                                      IList<PushDocument> p_DocumentBatch,
+                                      string p_SourceId)
+        {
+            if (p_DocumentBatch.Count == 0) {
+                return;
+            }
+
+            p_PlatformClient.DocumentManager.AddOrUpdateDocuments(p_SourceId, p_DocumentBatch, null);
+
+            p_DocumentBatch.Clear();
+        }
+
+        /// <summary>
+        /// Gets the push API endpoint URL to use that matches the command-line arguments specified.
+        /// </summary>
+        /// <param name="p_Args">Parsed command-line arguments.</param>
+        /// <returns>The push API endpoint URL to use.</returns>
+        private static string GetPushApiUrl(ProgramArguments p_Args)
+        {
+            switch (p_Args.Region) {
+                case CloudRegion.UsEast1:
+                    switch (p_Args.Environment) {
+                        case CloudEnvironment.Hipaa:
+                            return Constants.Endpoint.UsEast1.HIPAA_PUSH_API_URL;
+                        case CloudEnvironment.Prod:
+                            return Constants.Endpoint.UsEast1.PROD_PUSH_API_URL;
+                        case CloudEnvironment.QA:
+                            return Constants.Endpoint.UsEast1.QA_PUSH_API_URL;
+                        case CloudEnvironment.Dev:
+                            return Constants.Endpoint.UsEast1.DEV_PUSH_API_URL;
+                        default:
+                            throw new InvalidEnumArgumentException(INVALID_CLOUD_ENVIRONMENT);
+                    }
+                case CloudRegion.EuWest1:
+                    switch (p_Args.Environment) {
+                        case CloudEnvironment.Hipaa:
+                            throw new InvalidEnumArgumentException(HIPAA_INVALID_FOR_EUWEST1);
+                        case CloudEnvironment.Prod:
+                            return Constants.Endpoint.EuWest1.PROD_PUSH_API_URL;
+                        case CloudEnvironment.QA:
+                            return Constants.Endpoint.EuWest1.QA_PUSH_API_URL;
+                        case CloudEnvironment.Dev:
+                            return Constants.Endpoint.EuWest1.DEV_PUSH_API_URL;
+                        default:
+                            throw new InvalidEnumArgumentException(INVALID_CLOUD_ENVIRONMENT);
+                    }
+                default:
+                    throw new InvalidEnumArgumentException(INVALID_CLOUD_REGION);
+            }
+        }
+
+        /// <summary>
+        /// Gets the platform endpoint URL to use that matches the command-line arguments specified.
+        /// </summary>
+        /// <param name="p_Args">Parsed command-line arguments.</param>
+        /// <returns>The platform endpoint URL to use.</returns>
+        private static string GetPlatformApiUrl(ProgramArguments p_Args)
+        {
+            switch (p_Args.Region) {
+                case CloudRegion.UsEast1:
+                    switch (p_Args.Environment) {
+                        case CloudEnvironment.Hipaa:
+                            return Constants.PlatformEndpoint.UsEast1.HIPAA_PLATFORM_API_URL;
+                        case CloudEnvironment.Prod:
+                            return Constants.PlatformEndpoint.UsEast1.PROD_PLATFORM_API_URL;
+                        case CloudEnvironment.QA:
+                            return Constants.PlatformEndpoint.UsEast1.QA_PLATFORM_API_URL;
+                        case CloudEnvironment.Dev:
+                            return Constants.PlatformEndpoint.UsEast1.DEV_PLATFORM_API_URL;
+                        default:
+                            throw new InvalidEnumArgumentException(INVALID_CLOUD_ENVIRONMENT);
+                    }
+                case CloudRegion.EuWest1:
+                    switch (p_Args.Environment) {
+                        case CloudEnvironment.Hipaa:
+                            throw new InvalidEnumArgumentException(HIPAA_INVALID_FOR_EUWEST1);
+                        case CloudEnvironment.Prod:
+                            return Constants.PlatformEndpoint.EuWest1.PROD_PLATFORM_API_URL;
+                        case CloudEnvironment.QA:
+                            return Constants.PlatformEndpoint.EuWest1.QA_PLATFORM_API_URL;
+                        case CloudEnvironment.Dev:
+                            return Constants.PlatformEndpoint.EuWest1.DEV_PLATFORM_API_URL;
+                        default:
+                            throw new InvalidEnumArgumentException(INVALID_CLOUD_ENVIRONMENT);
+                    }
+                default:
+                    throw new InvalidEnumArgumentException(INVALID_CLOUD_REGION);
+            }
         }
     }
 }
---
+++ src/Coveo.Connectors.EasyFilePusher/ProgramArguments.cs
@@ -0,0 +1,106 @@
+﻿using CommandLine;
+
+namespace Coveo.Connectors.EasyFilePusher
+{
+    /// <summary>
+    /// Definition of the program input arguments.
+    /// </summary>
+    public class ProgramArguments
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Required = true, HelpText = "Cloud environment: " + nameof(CloudEnvironment.Hipaa) + ", " + nameof(CloudEnvironment.Prod) + ", " + nameof(CloudEnvironment.QA) + " or " + nameof(CloudEnvironment.Dev) + ".")]
+        public CloudEnvironment Environment { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Required = true, HelpText = "Cloud region: " + nameof(CloudRegion.UsEast1) + " or " + nameof(CloudRegion.EuWest1) + ".")]
+        public CloudRegion Region { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Required = true, HelpText = "ID of the organization in which to push documents.")]
+        public string Organization { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Required = true, HelpText = "ID of the source in which to push documents.")]
+        public string SourceId { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Required = true, HelpText = "API key to use.")]
+        public string ApiKey { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Required = true, HelpText = "Path of the local folder that contains the documents to index.")]
+        public string Folder { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Default = "*", HelpText = "Wildcard expression for which matching files will be pushed. All files are pushed by default")]
+        public string Include { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Default = true, HelpText = "Whether to recursively search in sub-folders for files to push. Sub-folders are searched by default.")]
+        public bool Recursive { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Option(Default = 10, HelpText = "How many files to push per batch.")]
+        public int BatchSize { get; set; }
+    }
+
+    /// <summary>
+    /// Cloud environments in which documents can be pushed.
+    /// </summary>
+    public enum CloudEnvironment
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Hipaa,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Prod,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        QA,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Dev
+    }
+
+    /// <summary>
+    /// Cloud regions in which documents can be pushed.
+    /// </summary>
+    public enum CloudRegion
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        UsEast1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        EuWest1
+    }
+}
